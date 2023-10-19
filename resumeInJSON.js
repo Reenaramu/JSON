c4c5cd1591f8a2f647e2f7139ee6c2a4441dc706
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 15:00:47 +0530

    Update resumeInJSON.js

--- resumeInJSON.js
+++ resumeInJSON.js
@@ -1,1 +1,51 @@
+//Create your own resume data in JSON format
+{
+  "objectives": [
+    "Pursue a career as a Full Stack Developer",
+    "Apply strong problem-solving skills to create efficient, scalable solutions",
+    "Contribute to a collaborative and innovative IT team"
+  ],
+  "name": "REENA R",
+  "title": "Aspiring Full Stack Developer",
+  "contact": {
+    "email": "reeff@gmail.com",
+    "address": "No.12 Washington Street, New York"
+  },
+  "summary": "Recent B.Tech graduate in Textile Technology with a strong desire to pursue a career in the IT field. Eager to apply problem-solving skills as a Full Stack Developer.",
+  "education": [
+    {
+      "degree": "B.Tech in Textile Technology",
+      "university": "Anna University",
+      "year": "2019-2023"
+    },
+    {
+      "degree": "HSE (Higher Secondary Education)",
+      "school": "Sri Sankara Vidyashramam Mat. Hr. Sec. School",
+      "year": "2019",
+      "percentage": "82.83%"
+    },
+    {
+      "degree": "SSLC (Secondary School Leaving Certificate)",
+      "school": "Sri Sankara Vidyashramam Mat. Hr. Sec. School",
+      "year": "2017",
+      "percentage": "91.4%"
+    }
+  ],
+  "skills": [
+    "Full Stack Web Development",
+    "Problem Solving",
+    "Teamwork",
+    "Communication",
+    "HTML/CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Database Management",
+    "Version Control (Git)"
+  ],
+  "college": {
+    "name": "Anna University",
+    "cgpa": "8.10"
+  }
+}
 
